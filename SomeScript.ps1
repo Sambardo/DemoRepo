9daa8c767a150c536939f7bc46f82635032a0b67
Kory T <korythacher@gmail.com>
2021-05-12 15:29:09 -0700

    added some junk to my script

--- SomeScript.ps1
+++ SomeScript.ps1
@@ -1,3 +1,5 @@
 Write-Host "Hello World"
 Write-Host "I changed this on my desktop"
-Write-Host "I changed this on github"
+Write-Host "I changed this on desktop"
+Write-Host "I changed this on desktop"
+Write-Host "I changed this on desktop"
