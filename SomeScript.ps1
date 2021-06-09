ffadcc910a1d2e688391c251db79713df9abe25a
Kory T <korythacher@gmail.com>
2021-06-09 07:22:54 -0700

    Resetting for demo later

--- SomeScript.ps1
+++ SomeScript.ps1
@@ -1,5 +1,1 @@
 Write-Host "Hello World"
-Write-Host "I changed this on my desktop"
-Write-Host "I changed this on desktop"
-Write-Host "I changed this on desktop"
-Write-Host "I changed this on desktop"
