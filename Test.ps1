38b7aff4c8c448e6cbbd1fce7b2f863fffec6b52
Kory T <korythacher@gmail.com>
2021-07-21 15:01:07 -0700

    starting my demo

---
+++ Test.ps1
@@ -0,0 +1,14 @@
+class VTest : System.Management.Automation.IValidateSetValuesGenerator
+{
+    [String[]] GetValidValues() { return ("a","b")}
+}
+
+function FunctionName {
+    param ([validateset([Vtest],ErrorMessage="You did this wrong")]$t
+    )
+
+    write-host $t -ForegroundColor Green
+    
+}
+
+FunctionName -t a
