e6fdc9b946e37ede0eba968bca536629f12500cc
Kory <KoryThacher@gmail.com>
2022-07-13 15:29:52 -0700

    Update SomeScript.ps1

--- SomeScript.ps1
+++ SomeScript.ps1
@@ -5,4 +5,18 @@ Function MyFunction
     Write-Host $message -ForegroundColor Green
 }
 
+Function MyFunction2
+{
+    param($message)
+
+    Write-Host $message -ForegroundColor Green
+}
+
+Function MyFunction3
+{
+    param($message)
+
+    Write-Host $message -ForegroundColor Green
+}
+
 MyFunction -message "Hello world"
