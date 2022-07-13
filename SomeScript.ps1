a71769c95cb4450ffd4d7889f6ed3340caec3aab
Kory T <korythacher@gmail.com>
2022-07-13 15:28:27 -0700

    Doing stuff in vscode

--- SomeScript.ps1
+++ SomeScript.ps1
@@ -1,8 +1,15 @@
+
+Write-Host $message -ForegroundColor Green
 Function MyFunction
 {
     param($message)
 
     Write-Host $message -ForegroundColor Green
+    Write-Host $message -ForegroundColor yellow
 }
 
 MyFunction -message "Hello world"
+
+Write-Host $message -ForegroundColor Green
+
+Write-Host $message -ForegroundColor Green
