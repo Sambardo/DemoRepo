0934cf8a9df87075269ac1b0ec423e040a172d34
Kory T <korythacher@gmail.com>
2022-07-18 11:40:12 -0700

    some junk

--- SomeScript.ps1
+++ SomeScript.ps1
@@ -9,22 +9,3 @@ Function MyFunction
 }
 
 MyFunction -message "Hello world"
-
-Write-Host $message -ForegroundColor Green
-
-Write-Host $message -ForegroundColor Green
-Function MyFunction2
-{
-    param($message)
-
-    Write-Host $message -ForegroundColor Green
-}
-
-Function MyFunction3
-{
-    param($message)
-
-    Write-Host $message -ForegroundColor Green
-}
-
-MyFunction -message "Hello world"
